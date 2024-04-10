1a6d2e72f0c86118378cc39d7fe9ffb9eab0e0a7
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-10 11:49:11 +0300

    added categories

--- Apps.Pinecone/PineconeApplication.cs
+++ Apps.Pinecone/PineconeApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Pinecone;
 
-public class PineconeApplication : IApplication
+public class PineconeApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.ArtificialIntelligence];
+        set { }
+    }
+    
     public string Name
     {
         get => "Pinecone";
